c3d02b811f6d5100d44c306bd69126a7474aac46
Benjamin Wohlwend <piquadrat@gmail.com>
2013-02-14 13:30:14 +0100

    nicer admin, verbose names, some pep8 cleanup

--- shop_postfinance/admin.py
+++ shop_postfinance/admin.py
@@ -3,4 +3,11 @@
 from django.contrib import admin
 from shop_postfinance.models import PostfinanceIPN
 
-admin.site.register(PostfinanceIPN) # This will be a little ugly... but necessary
+
+class PostFinanceIPNAdmin(admin.ModelAdmin):
+    list_display = 'orderID', 'CN', 'amount', 'currency', 'PM', 'STATUS', 'created_at'
+    list_filter = 'currency', 'STATUS', 'PM'
+    date_hierarchy = 'created_at'
+    readonly_fields = 'orderID', 'currency', 'amount', 'PM', 'ACCEPTANCE', 'STATUS', 'CARDNO', 'CN', 'TRXDATE', 'PAYID', 'NCERROR', 'BRAND', 'IPCTY', 'CCCTY', 'ECI', 'CVCCheck', 'AAVCheck', 'VC', 'IP', 'SHASIGN', 'updated_at', 'created_at'
+    search_fields = 'orderID', 'CN'
+admin.site.register(PostfinanceIPN, PostFinanceIPNAdmin)
--- shop_postfinance/models.py
+++ shop_postfinance/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+PAYMENT_STATUS = (
+    ('5', _('Authorized')),
+    ('9', _('Payment requested')),
+    ('0', _('Invalid or incomplete')),
+    ('2', _('Authorization refused')),
+    ('51', _('Authorization waiting')),
+    ('52', _('Authorisation not known')),
+    ('91', _('Payment processing')),
+    ('92', _('Payment uncertain')),
+    ('93', _('Payment refused')),
+)
 
 
 class PostfinanceIPN(models.Model):
     """
-    A simple, dumb model that traces all the IPNs recieved from postfinance,
+    A simple, dumb model that traces all the IPNs received from postfinance,
     for archiving reasons.
     
     This isn't really human friends, but should still be kept in the database
@@ -12,27 +26,34 @@ class PostfinanceIPN(models.Model):
     
     I am not a lawyer :)
     """
-    orderID = models.CharField(max_length=255)#=Test27&
-    currency = models.CharField(max_length=255)#=CHF&
-    amount = models.CharField(max_length=255) #=54&
-    PM = models.CharField(max_length=255) #=CreditCard&
-    ACCEPTANCE = models.CharField(max_length=255) #=test123&
-    STATUS = models.CharField(max_length=255) #=9&
-    CARDNO = models.CharField(max_length=255) #=XXXXXXXXXXXX3333&ED=0317&
-    CN= models.CharField(max_length=255) #Testauzore+Testos&
-    TRXDATE = models.CharField(max_length=255) #=11/08/10&
-    PAYID = models.CharField(max_length=255) #=8628366&
-    NCERROR = models.CharField(max_length=255) #=0&
-    BRAND = models.CharField(max_length=255) #=VISA&
-    IPCTY = models.CharField(max_length=255) #=CH&
-    CCCTY = models.CharField(max_length=255) #=US&
-    ECI = models.CharField(max_length=255) #=7&
-    CVCCheck = models.CharField(max_length=255) #=NO&
-    AAVCheck = models.CharField(max_length=255) #=NO&
-    VC = models.CharField(max_length=255) #=NO&
-    IP = models.CharField(max_length=255) #=84.226.127.220&
-    SHASIGN = models.CharField(max_length=255) #=CEE483B0557B8E3437A55094221E15C7DB6A0D63
+    orderID = models.CharField(_('Order ID'), max_length=255)  #=Test27&
+    currency = models.CharField(_('currency'), max_length=255)  #=CHF&
+    amount = models.CharField(_('Amount'), max_length=255)   #=54&
+    PM = models.CharField(_('Payment method'), max_length=255)  #=CreditCard&
+    ACCEPTANCE = models.CharField(_('Authorization code'), max_length=255)  #=test123&
+    STATUS = models.CharField(_('status'), max_length=255, choices=PAYMENT_STATUS)  #=9&
+    CARDNO = models.CharField(_('Credit Card Number'), max_length=255)  #=XXXXXXXXXXXX3333&ED=0317&
+    CN = models.CharField(_('Customer Name'), max_length=255)  #Testauzore+Testos&
+    TRXDATE = models.CharField(_('Transaction Date'), max_length=255)  #=11/08/10&
+    PAYID = models.CharField(_('Payment ID'), max_length=255)  #=8628366&
+    NCERROR = models.CharField(_('Error code'), max_length=255)  #=0&
+    BRAND = models.CharField(_('Card brand'), max_length=255)  #=VISA&
+    IPCTY = models.CharField(max_length=255)  #=CH&
+    CCCTY = models.CharField(max_length=255)  #=US&
+    ECI = models.CharField(max_length=255)  #=7&
+    CVCCheck = models.CharField(max_length=255)  #=NO&
+    AAVCheck = models.CharField(max_length=255)  #=NO&
+    VC = models.CharField(max_length=255)  #=NO&
+    IP = models.CharField(max_length=255)  #=84.226.127.220&
+    SHASIGN = models.CharField(max_length=255)  #=CEE483B0557B8E3437A55094221E15C7DB6A0D63
     
     # Timestamping
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return u'%s (%s %s, %s)' % (self.orderID, self.amount, self.currency, self.PM)
+
+    class Meta:
+        verbose_name = _('PostFinance IPN')
+        verbose_name_plural = _('PostFinance IPNs')
