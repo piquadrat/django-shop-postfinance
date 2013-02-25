0189f82b156ac14038fdc7a448661bd1f382d6f7
Benjamin Wohlwend <piquadrat@gmail.com>
2013-02-25 14:47:27 +0100

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Christopher Glass",
     author_email="tribaal@gmail.com",
     name='django-shop-postfinance',
-    version='0.3',
+    version='0.3.1',
     description='A postfinance payment backend for django SHOP',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     url='http://www.django-shop.org/',
